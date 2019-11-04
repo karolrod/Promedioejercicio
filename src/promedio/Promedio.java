e2291694d89243ec3ab3927e6e601504fe80afca
karolrod <zlshopml@gmail.com>
2019-11-03 21:47:26 -0300

    cambios

---
+++ src/promedio/Promedio.java
@@ -0,0 +1,23 @@
+package promedio;
+
+import java.util.Scanner;
+
+public class Promedio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ingreso = new Scanner(System.in);
+		System.out.println("Ingrese el valor 1:");
+		int valor1 = ingreso.nextInt();
+		System.out.println("Ingrese el valor 2:");
+		int valor2 = ingreso.nextInt();
+		System.out.println("Ingrese el valor 3:");
+		int valor3 = ingreso.nextInt();
+		int total = valor1 + valor2 + valor3;
+		int promedio = total / 3;
+		System.out.println("El promedio es: " + promedio);
+		ingreso.close();
+
+	}
+
+}
